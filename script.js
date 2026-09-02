3fb92a32b82fd4bd673821194bd345390bcdf6b6
witherserik58-gif <witherserik58@gmail.com>
2026-09-02 18:55:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const videocards = document.querySelectorAll(".box1");
 
+videocards.forEach(function(card) {
+    card.addEventListener("click", function() {
+        window.location.href = "videocard/videocard.html";
+    });
+});
